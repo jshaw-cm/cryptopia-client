1b7ee368787c29cf75e3f81db3df74bb3991c86b
Justin Shaw <jshaw1@amfam.com>
2017-06-01 12:20:26 -0500

    Completely redone to improve readability and make it all around easier to use.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # cryptopia-client
+
+The only methods that are currently finished and working are:
+Private:
+		GetBalance
+
+Public:
+		GetCurrencies
+
+I have not tested sending params, so you should not expect anything to work if you send params..
+
+Ex:
+npm install --save cryptopia-client
+
+var CryptopiaClient = require("cryptopia-client");
+
+(These keys can be created at https://www.cryptopia.co.nz/Security when you are logged into your cryptopia account...)
+
+var keys = {
+	api_key: "get your own damn api key",
+	secret_key: "get your own damn secret key"
+}
+
+var client = new CryptopiaClient(keys);
+
+client.GetBalance(callback);
+
+function callback(error, data) {
+	if(error) console.log("E!",error)
+
+	console.log("Data: ", data);
+}
+
+or
+
+client.GetCurrencies(callback);
+
+function callback(error, data) {
+	if(error) console.log("Error", error)
+
+	console.log("Data: ", data);
+}
+
--- client.js
+++ client.js
@@ -1,230 +1,117 @@
-var request		= require('request');
-var crypto		= require('crypto');
-var querystring	= require('querystring');
+var	https 				= require("https"),
+		crypto 				= require("crypto");
 
-/**
- * cryptopiaClient connects to the cryptopia.com API
- * @param {String} key    API Key
- * @param {String} secret API Secret
- * @param {String} [otp]  Two-factor password (optional) (also, doesn't work)
- */
-function CryptopiaClient(keys, otp, params) {
-	var self = this;
+function CryptopiaClient(keys){
+	var self 		= this;
+	self.keys 	= keys;
 
-	var config = {
-		url: 'https://www.cryptopia.co.nz',
-		version: 'api',
-		key: keys.api_key,
-		secret: keys.secret_key,
-		otp: otp,
-		timeoutMS: 5000,
-		nonce: Math.floor(new Date().getTime() / 1000)
+	function fetch_data(api_method, callback, params) {
+		var methods = {
+			private: 'GetBalance',
+			public: 'GetCurrencies'
+		}
+
+		//If it's a public method, make public request
+		if(methods.public.indexOf(api_method) > -1){
+			return public_request(callback, api_method, params);
+		} 
+
+		//If it's a private method, make a private request
+		else if(methods.private.indexOf(api_method) > -1){
+			//If any keys are missing, throw an error...
+			if(!self.keys.api_key || !self.keys.secret_key){
+				throw new Error("I couldn't find one of the keys.");
+			} else {
+				return private_request(callback, api_method, params);
+			}
+		} 
+
+		//Throw error if the api method doesn't exist
+		else {
+			throw new Error("The api method '" + api_method + "' isn't currently supported by this client.");
+		}
+	}
+
+	function public_request(callback, api_method) {
+		var params = params || {}
+
+		var options = {
+			host: 'www.cryptopia.co.nz',
+			path: '/Api/' + api_method,
+			method: 'GET'
+		}
+
+		var req = https.request(options, (res) => {
+		  res.setEncoding('utf8');
+		  res.on('data', (chunk) => {
+				callback.call(this, null, chunk);
+		  }); 	
+		});
+
+		req.on('error', (e) => {
+			callback.call(this, e, null);
+		});
+
+		req.end();
+	}
+
+	function private_request(callback, api_method) {
+		var params = params || {}
+		var amx_value = generate_amx(params, api_method);
+
+		var headers = { 
+		 'Authorization': amx_value, 
+		 'Content-Type':'application/json; charset=utf-8',
+		};
+
+		var options = {
+			host: 'www.cryptopia.co.nz',
+			path: '/Api/' + api_method,
+			method: 'POST',
+			headers: headers
+		}
+		console.log(options)
+		var req = https.request(options, (res) => {
+			var json = null;
+		  res.setEncoding('utf8');
+		  res.on('data', (chunk) => {
+				json = chunk;
+		  });
+		  res.on('end', () => {
+		  	callback.call(this, null, json);
+		  })
+		});
+
+		req.on('error', (e) => {
+			callback.call(this, e, null);
+		});
+
+		//write data to request body
+		//This wouldn't work until I used this line...
+		req.write(JSON.stringify(params));
+		req.end();
+
+	}
+
+	function generate_amx(params, api_method) {
+			//I made this function out of a block of code that can be found here https://www.cryptopia.co.nz/Forum/Thread/262
+			var nonce = Math.floor(new Date().getTime() / 1000);
+			var md5 = crypto.createHash('md5').update( JSON.stringify( params ) ).digest();
+			var requestContentBase64String = md5.toString('base64');
+			var signature = self.keys.api_key + "POST" + encodeURIComponent( 'https://www.cryptopia.co.nz/Api/' + api_method ).toLowerCase() + nonce + requestContentBase64String;
+			var hmacsignature = crypto.createHmac('sha256', new Buffer( self.keys.secret_key, "base64" ) ).update( signature ).digest().toString('base64');
+			var amx = "amx " + self.keys.api_key + ":" + hmacsignature + ":" + nonce;
+
+			return amx;
+	}
+
+	self.GetCurrencies = (callback, params) => {
+		fetch_data('GetCurrencies', callback);
 	};
 
-	/**
-	 * This method makes a public or private API request.
-	 * @param  {String}   method   The API method (public or private)
-	 * @param  {Object}   params   Arguments to pass to the api call
-	 * @param  {Function} callback A callback function to be executed when the request is complete
-	 * @return {Object}            The request object
-	 */
-	function api(method, params, callback) {
-		var methods = {
-			public: ['GetCurrencies', 'GetTradePairs', 'GetMarkets', 'GetMarket', 'GetMarketHistory', 'GetMarketOrders'],
-			private: ['GetBalance', 'GetDepositAddress', 'GetOpenOrders', 'GetTradeHistory', 'GetTransactions', 'SubmitTrade', 'CancelTrade', 'SubmitTip']
-		};
-
-		if(params === null){
-			params = {};
-		}
-
-		if(methods.public.indexOf(method) !== -1) {
-			return publicMethod(method, params, callback);
-		}
-		else if(methods.private.indexOf(method) !== -1) {
-			return privateMethod(method, params, callback);
-		}
-		else {
-			throw new Error(method + ' is not a valid API method.');
-		}
-	}
-
-	/**
-	 * This method makes a public API request.
-	 * @param  {String}   method   The API method (public or private)
-	 * @param  {Object}   params   Arguments to pass to the api call
-	 * @param  {Function} callback A callback function to be executed when the request is complete
-	 * @return {Object}            The request object
-	 */
-	function publicMethod(method, params, callback) {
-		params = params || {};
-
-		var path	= '/' + config.version + '/' + method;
-		var url		= config.url + path;
-
-		return rawGetRequest(url, {}, params, callback);
-	}
-
-	/**
-	 * This method makes a private API request.
-	 * @param  {String}   method   The API method (public or private)
-	 * @param  {Object}   params   Arguments to pass to the api call
-	 * @param  {Function} callback A callback function to be executed when the request is complete
-	 * @return {Object}            The request object
-	 */
-	function privateMethod(method, params, callback) {
-		var path	= '/' + config.version + '/' + method;
-		var url		= config.url + path;
-
-		//params.nonce = new Date() * 1000; // spoof microsecond
-		var signature = getMessageSignature(url, params, config.nonce);
-
-		var headers = {
-			'Authorization': signature
-		};
-
-		return rawRequest(url, headers, params, callback);
-	}
-
-	/**
-	 * This method returns a signature for a request as a Base64-encoded string
-	 * @param  {String}  path    The relative URL path for the request
-	 * @param  {Object}  request The POST body
-	 * @param  {Integer} nonce   A unique, incrementing integer
-	 * @return {String}          The request signature
-	 */
-	function getMessageSignature(url, params, n) {
-		var nonce = Math.floor(new Date().getTime() / 1000);
-		var md5 = crypto.createHash('md5').update( JSON.stringify( params ) ).digest();
-		var requestContentBase64String = md5.toString('base64');
-		//console.log(requestContentBase64String)
-		//EVERYTHING IS THE SAME AT THIS POINT! (except)
-		var signature = config.key + "POST" + encodeURIComponent( url ).toLowerCase() + nonce + requestContentBase64String;
-		var hmacsignature = crypto.createHmac('sha256', new Buffer( config.secret, "base64" ) ).update( signature ).digest().toString('base64');
-		var header_value = "amx " + config.key + ":" + hmacsignature + ":" + nonce;
-
-		/*var message	= querystring.stringify(request);
-		var secret	= new Buffer(config.secret, 'base64');
-		var hash	= new crypto.createHash('sha256');
-		var hmac	= new crypto.createHmac('sha512', secret);
-
-		var hash_digest	= hash.update(nonce + message).digest('binary');
-		var hmac_digest	= hmac.update(path + hash_digest, 'binary').digest('base64');*/
-
-		return header_value;
-	}
-
-	/**
-	 * This method sends the actual HTTP request
-	 * @param  {String}   url      The URL to make the request
-	 * @param  {Object}   headers  Request headers
-	 * @param  {Object}   params   POST body
-	 * @param  {Function} callback A callback function to call when the request is complete
-	 * @return {Object}            The request object
-	 */
-	function rawRequest(url, headers, params, callback) {
-		// Set custom User-Agent string
-		headers['Content-Type'] = 'application/json; charset=utf-8';
-
-		var options = {
-			host: "www.cryptopia.co.nz",
-			path: '/api/GetBalance',
-			method: 'POST',
-			url: url,
-			headers: headers
-		};
-
-		console.log(options)
-
-		var req = request.post(options, function(error, response, body) {
-			if(typeof callback === 'function') {
-				var data;
-
-				if(error) {
-					return callback.call(self, new Error('Error in server response: ' + JSON.stringify(error) + " :response: " + error), null);
-				}
-
-				try {
-					data = JSON.parse(body);
-				}
-				catch(e) {
-					return callback.call(self, new Error('Could not understand response from server: ' + JSON.stringify(response.body)), null);
-				}
-				//If any errors occured, cryptopia will give back an array with error strings under
-				//the key "error". We should then propagate back the error message as a proper error.
-				if(data.error && data.error.length) {
-					var cryptopiaError = null;
-					data.error.forEach(function(element) {
-						if (element.charAt(0) === "E") {
-							cryptopiaError = element.substr(1);
-							return false;
-						}
-					});
-					if (cryptopiaError) {
-						return callback.call(self, new Error('Cryptopia API returned error: ' + cryptopiaError), null);
-					}
-				}
-				else {
-					return callback.call(self, null, data);
-				}
-			}
-		});
-		return req;
-	}
-
-	function rawGetRequest(url, headers, params, callback) {
-		// Set custom User-Agent string
-		headers['Content-Type'] = 'application/json; charset=utf-8';
-
-		var options = {
-			url: url,
-			method: 'GET',
-			headers: headers,
-			form: params,
-			timeout: config.timeoutMS
-		};
-
-		console.log(options)
-
-		var req = request.get(options, function(error, response, body) {
-			if(typeof callback === 'function') {
-				var data;
-
-				if(error) {
-					return callback.call(self, new Error('Error in server response: ' + JSON.stringify(error) + " :response: " + error), null);
-				}
-
-				try {
-					data = JSON.parse(body);
-				}
-				catch(e) {
-					return callback.call(self, new Error('Could not understand response from server: ' + body), null);
-				}
-				//If any errors occured, cryptopia will give back an array with error strings under
-				//the key "error". We should then propagate back the error message as a proper error.
-				if(data.error && data.error.length) {
-					var cryptopiaError = null;
-					data.error.forEach(function(element) {
-						if (element.charAt(0) === "E") {
-							cryptopiaError = element.substr(1);
-							return false;
-						}
-					});
-					if (cryptopiaError) {
-						return callback.call(self, new Error('Cryptopia API returned error: ' + cryptopiaError), null);
-					}
-				}
-				else {
-					return callback.call(self, null, data);
-				}
-			}
-		});
-		return req;
-	}
-
-	self.api			= api;
-	self.publicMethod	= publicMethod;
-	self.privateMethod	= privateMethod;
+	self.GetBalance = (callback) => {
+		fetch_data('GetBalance', callback);
+	};
 }
 
 module.exports = CryptopiaClient;
